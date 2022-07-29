09ae9795287da2e7fc812114b5b1358451483fb8
Thiago Melo <tbgdmelo@gmail.com>
2022-07-28 23:26:37 -0400

    local

--- services/api.js
+++ services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://192.168.0.22:8002',
+  baseURL: 'http://localhost:8002',
   headers: {
     'Content-Type': 'application/json',
     Accept: 'application/json',
